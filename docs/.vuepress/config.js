7480dc61db2f67a31a4758b0fd008bb37426a0bd
Andy Fusniak <andy@andyfusniak.com>
2019-05-21 16:17:53 +0100

    Fleshed out structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   title: 'Ecom',
-  description: 'Documentation and getting started guides for Ecom CLI Tool and JS Client Library.'
+  description: 'Documentation and getting started guides for Ecom CLI Tool and JS Client Library.',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'CLI Tool', link: '/cli-tool/' },
+      { text: 'JS Client Library', link: '/jsclient/' },
+    ],
+    sidebar: 'auto'
+  }
 }
---
+++ docs/cli-tool/README.md
@@ -0,0 +1,17 @@
+# Ecom CLI Tool
+
+## Installation
+### Mac
+[Download latest ecom for Mac](/downloads/mac/ecom)
+
+After you've downloaded the executable move
+
+### Windows
+[Download latest ecom.exe for Windows](/downloads/windows/ecom.exe)
+
+### Linux
+[Download latest ecom for Linux](/downloads/linux/ecom)
+
+
+## Getting Started
+
